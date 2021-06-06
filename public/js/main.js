58774db7d7d7a6f35cb85cc2eebce5304249fc0a
nestorpons <nestorpons@gmail.com>
2021-06-06 21:12:59 +0200

    varios

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,16 @@
 // Carga pagina principal
-
+$('#main_content').load('/dash.html')
 // Comportamiento de los hipervínculos
+
 $('.intlink').on('click', function(e) {
     e.stopPropagation()
     e.preventDefault()
-    c = $(this).attr('href')
+    let c = $(this).attr('href')
     $('#main_content').load(c)
-
+})
+$('.outlink').on('click', function(e) {
+    e.stopPropagation()
+    e.preventDefault()
+    let c = $(this).attr('href')
+    $('#main_content').html(`<iframe id="main_frame" src="${c}" name="myIframe" frameborder="" width="100%" height="100%" allowfullscreen></iframe>`)
 })
